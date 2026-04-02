de7e055753eeb01ec9f3115e386b68aa5d5595de
Yadhu Krishna <yadhukrishnan0718@gmail.com>
2026-04-02 17:29:56 +0530

    Fix Vite build: Include case-study pages

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        caseStudy: resolve(__dirname, 'case-study.html'),
+        caseStudy0: resolve(__dirname, 'case-study0.html'),
+        caseStudy1: resolve(__dirname, 'case-study1.html'),
+        caseStudy2: resolve(__dirname, 'case-study2.html'),
+        caseStudy3: resolve(__dirname, 'case-study3.html'),
+      }
+    }
+  }
+});
